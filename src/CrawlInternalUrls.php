f873fca197a1eaee0ebc12a1beeb92eb19796a7e
Ioannis Lazaridis <ioannis.lazaridis@gmail.com>
2017-01-24 16:02:08 +0100

    Fix styling

--- src/CrawlInternalUrls.php
+++ src/CrawlInternalUrls.php
@@ -5,10 +5,12 @@ namespace Spatie\Crawler;
 class CrawlInternalUrls implements CrawlProfile
 {
     protected $host = '';
+
     public function __construct(string $baseUrl)
     {
         $this->host = parse_url($baseUrl, PHP_URL_HOST);
     }
+
     public function shouldCrawl(Url $url): bool
     {
         return $this->host === $url->host ? true : false;
