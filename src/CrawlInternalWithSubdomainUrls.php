be6a83edf0031bad3392da4ce38cd9bdf6c86fd4
Brent Robert <brentrobert@me.com>
2017-10-04 11:29:38 +0200

    Removed dependency

--- src/CrawlInternalWithSubdomainUrls.php
+++ src/CrawlInternalWithSubdomainUrls.php
@@ -2,26 +2,17 @@
 
 namespace Spatie\Crawler;
 
-use LayerShifter\TLDExtract\Extract;
-
 class CrawlInternalWithSubdomainUrls implements CrawlProfile
 {
     protected $host = '';
 
-    /** @var Extract */
-    protected $extract;
-
     public function __construct(string $baseUrl)
     {
         $this->host = parse_url($baseUrl, PHP_URL_HOST);
-        $this->extract = new Extract();
     }
 
     public function shouldCrawl(Url $url): bool
     {
-        $baseUrl = $this->extract->parse($this->host);
-        $currentUrl = $this->extract->parse($url);
-
-        return $baseUrl->getHostname() === $currentUrl->getHostname();
+        return substr($url->host, -strlen($this->host)) === $this->host;
     }
 }
