e106c250d7c7a4904c8f898e34646540a2bb14af
freek <freek@spatie.be>
2018-06-05 10:24:29 +0200

    clean up test

--- tests/CrawlObserverCollectionTest.php
+++ tests/CrawlObserverCollectionTest.php
@@ -14,23 +14,24 @@ use GuzzleHttp\Exception\RequestException;
 
 class CrawlObserverCollectionTest extends TestCase
 {
-    private $observer;
-
-    public static $crawled = false;
-
-    public static $failed = false;
+    /** @var \Spatie\Crawler\CrawlObserver */
+    protected $crawlObserver;
 
     protected function setUp()
     {
         parent::setUp();
 
-        $this->observer = new class extends CrawlObserver {
+        $this->crawlObserver = new class extends CrawlObserver {
+            public $crawled = false;
+
+            public $failed = false;
+
             public function crawled(
                 UriInterface $url,
                 ResponseInterface $response,
                 ?UriInterface $foundOnUrl = null
             ) {
-                CrawlObserverCollectionTest::$crawled = true;
+                $this->crawled = true;
             }
 
             public function crawlFailed(
@@ -38,41 +39,42 @@ class CrawlObserverCollectionTest extends TestCase
                 RequestException $requestException,
                 ?UriInterface $foundOnUrl = null
             ) {
-                CrawlObserverCollectionTest::$failed = true;
+                $this->failed = true;
             }
         };
-
-        self::$crawled = false;
-        self::$failed = false;
     }
 
     /** @test */
-    public function it_can_be_fulfill()
+    public function it_can_be_fulfilled()
     {
         $observers = new CrawlObserverCollection([
-            $this->observer,
+            $this->crawlObserver,
         ]);
 
-        $observers->crawled(CrawlUrl::create(new Uri('')), new Response());
+        $observers->crawled(
+            CrawlUrl::create(new Uri('')),
+            new Response()
+        );
 
-        $this->assertTrue(self::$crawled);
-
-        $this->assertFalse(self::$failed);
+        $this->assertTrue($this->crawlObserver->crawled);
+        $this->assertFalse($this->crawlObserver->failed);
     }
 
     /** @test */
     public function it_can_fail()
     {
         $observers = new CrawlObserverCollection([
-            $this->observer,
+            $this->crawlObserver,
         ]);
 
         $uri = new Uri('');
 
-        $observers->crawlFailed(CrawlUrl::create($uri), new RequestException('', new Request('GET', $uri)));
+        $observers->crawlFailed(
+            CrawlUrl::create($uri),
+            new RequestException('', new Request('GET', $uri))
+        );
 
-        $this->assertFalse(self::$crawled);
-
-        $this->assertTrue(self::$failed);
+        $this->assertFalse($this->crawlObserver->crawled);
+        $this->assertTrue($this->crawlObserver->failed);
     }
 }
