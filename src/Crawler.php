2080f1187cb48b0a26a02c6469af382e47040fe3
Lorenzo Scontrino <lorenzo.scontrino@noerdisch.de>
2018-06-07 14:54:38 +0200

    fixed respect robots condtion

--- src/Crawler.php
+++ src/Crawler.php
@@ -292,7 +292,9 @@ class Crawler
 
         $this->robotsTxt = $this->createRobotsTxt($crawlUrl->url);
 
-        if ($this->robotsTxt->allows((string) $crawlUrl->url)) {
+        if ($this->robotsTxt->allows((string) $crawlUrl->url) || 
+            !$this->respectRobots
+        ) {
             $this->addToCrawlQueue($crawlUrl);
         }
 
