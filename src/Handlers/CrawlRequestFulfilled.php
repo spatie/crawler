494d84043fabe3fc098f1e76b18243e01802f971
Freek Van der Herten <freek@spatie.be>
2018-06-25 10:09:31 +0200

    Apply fixes from StyleCI (#172)

--- src/Handlers/CrawlRequestFulfilled.php
+++ src/Handlers/CrawlRequestFulfilled.php
@@ -3,10 +3,10 @@
 namespace Spatie\Crawler\Handlers;
 
 use Spatie\Crawler\Crawler;
-use Spatie\Crawler\CrawlerRobots;
-use Spatie\Crawler\CrawlSubdomains;
 use Spatie\Crawler\CrawlUrl;
 use Spatie\Crawler\LinkAdder;
+use Spatie\Crawler\CrawlerRobots;
+use Spatie\Crawler\CrawlSubdomains;
 use Psr\Http\Message\StreamInterface;
 use Psr\Http\Message\ResponseInterface;
 
