f073be154e7d7bd3fa1f66535b92a1a0bbe05f51
Carlos Monroy Fernández <cmonroy@boardfy.com>
2019-05-23 09:52:35 +0200

    Making StyleCi happy again.

--- src/Handlers/CrawlRequestFulfilled.php
+++ src/Handlers/CrawlRequestFulfilled.php
@@ -3,15 +3,15 @@
 namespace Spatie\Crawler\Handlers;
 
 use GuzzleHttp\Psr7\Uri;
-use GuzzleHttp\RedirectMiddleware;
-use Psr\Http\Message\ResponseInterface;
-use Psr\Http\Message\StreamInterface;
-use Psr\Http\Message\UriInterface;
 use Spatie\Crawler\Crawler;
-use Spatie\Crawler\CrawlerRobots;
-use Spatie\Crawler\CrawlSubdomains;
 use Spatie\Crawler\CrawlUrl;
 use Spatie\Crawler\LinkAdder;
+use Spatie\Crawler\CrawlerRobots;
+use GuzzleHttp\RedirectMiddleware;
+use Psr\Http\Message\UriInterface;
+use Spatie\Crawler\CrawlSubdomains;
+use Psr\Http\Message\StreamInterface;
+use Psr\Http\Message\ResponseInterface;
 use function GuzzleHttp\Psr7\stream_for;
 
 class CrawlRequestFulfilled
