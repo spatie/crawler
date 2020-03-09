518f12ff22710e5ddc6d514b3fbba15b42bff30c
Mattias Geniar <m@ttias.be>
2020-03-09 13:18:53 +0100

    Bugfix: respect the getMaximumResponseSize() when passing the body-content on to the RobotsMeta checks

--- src/CrawlerRobots.php
+++ src/CrawlerRobots.php
@@ -17,11 +17,11 @@ class CrawlerRobots
     /** @var bool */
     protected $mustRespectRobots;
 
-    public function __construct(ResponseInterface $response, bool $mustRespectRobots)
+    public function __construct(array $headers, string $body, bool $mustRespectRobots)
     {
-        $this->robotsHeaders = RobotsHeaders::create($response->getHeaders());
+        $this->robotsHeaders = RobotsHeaders::create($headers);
 
-        $this->robotsMeta = RobotsMeta::create((string) $response->getBody());
+        $this->robotsMeta = RobotsMeta::create($body);
 
         $this->mustRespectRobots = $mustRespectRobots;
     }
