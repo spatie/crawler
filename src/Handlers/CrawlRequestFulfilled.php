b91513b32ab7e8d2c0b1c0eeb5c2ea233cd2475e
Mattias Geniar <m@ttias.be>
2020-06-08 11:53:52 +0200

    BodyStream: read the data in chunks (#303)
    * BodyStream: read the data in chunks
    
    * style
    
    * Add exception handling for "Unable to read from stream" errors

--- src/Handlers/CrawlRequestFulfilled.php
+++ src/Handlers/CrawlRequestFulfilled.php
@@ -90,7 +90,23 @@ class CrawlRequestFulfilled
             $bodyStream->rewind();
         }
 
-        $body = $bodyStream->read($readMaximumBytes);
+        $body = '';
+
+        $chunksToRead = $readMaximumBytes < 512 ? $readMaximumBytes : 512;
+
+        for ($bytesRead = 0; $bytesRead < $readMaximumBytes; $bytesRead += $chunksToRead) {
+            try {
+                $newDataRead = $bodyStream->read($chunksToRead);
+            } catch (Exception $e) {
+                $newDataRead = null;
+            }
+
+            if (! $newDataRead) {
+                break;
+            }
+
+            $body .= $newDataRead;
+        }
 
         return $body;
     }
