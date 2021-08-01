864731d050030f2ae580200b9fb02bd6926fa1aa
Daniel Freitas <daniel1208x@gmail.com>
2021-08-01 02:55:46 -0300

    change response type hint

--- src/ResponseWithCachedBody.php
+++ src/ResponseWithCachedBody.php
@@ -3,12 +3,13 @@
 namespace Spatie\Crawler;
 
 use GuzzleHttp\Psr7\Response;
+use Psr\Http\Message\ResponseInterface;
 
 class ResponseWithCachedBody extends Response
 {
     protected ?string $cachedBody = null;
 
-    public static function fromGuzzlePsr7Response(Response $response): static
+    public static function fromGuzzlePsr7Response(ResponseInterface $response): static
     {
         return new static(
             $response->getStatusCode(),
