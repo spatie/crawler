e818d20cc8d98717e4f4ab4798051d8be82c9e99
Voltra <25990549+Voltra@users.noreply.github.com>
2025-10-20 21:09:39 +0000

    Fix tests for pest 2.0.0

--- tests/CrawlUrlTest.php
+++ tests/CrawlUrlTest.php
@@ -1,7 +1,6 @@
 <?php
 
 use GuzzleHttp\Psr7\Uri;
-use Spatie\Crawler\Crawler;
 use Spatie\Crawler\CrawlUrl;
 use Spatie\Crawler\Test\TestClasses\Log;
 
@@ -11,17 +10,15 @@ beforeEach(function () {
     Log::reset();
 });
 
-describe('::create($url, ?$foundOnUrl, ?$id, ?$linkText)', function () {
-    it('creates a CrawlUrl instance that isn\'t in an invalid state when no $id is provided', function () {
-        $crawlUrl = CrawlUrl::create(
-            url: new Uri('/some/test-uri'),
-            foundOnUrl: new Uri('/'),
-            linkText: 'Some link text',
-        );
+it('creates a CrawlUrl instance that isn\'t in an invalid state when no $id is provided', function () {
+    $crawlUrl = CrawlUrl::create(
+        url: new Uri('/some/test-uri'),
+        foundOnUrl: new Uri('/'),
+        linkText: 'Some link text',
+    );
 
-        expect($crawlUrl)->toBeInstanceOf(CrawlUrl::class);
+    expect($crawlUrl)->toBeInstanceOf(CrawlUrl::class);
 
-        expect(fn() => $crawlUrl->getId())->not->toThrow(\Throwable::class);
-        expect(fn() => $crawlUrl->getId())->not->toThrow(\Error::class);
-    });
+    expect(fn() => $crawlUrl->getId())->not->toThrow(\Throwable::class);
+    expect(fn() => $crawlUrl->getId())->not->toThrow(\Error::class);
 });
